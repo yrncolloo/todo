31efd49befdc24f036eb8537c5f30e0b18b6d15e
yrncollo <ngenocollins599@gmail.com>
2024-01-24 12:11:08 +0300

    modified the heading functionality

--- src/components/atom/heading_one.rs
+++ src/components/atom/heading_one.rs
@@ -1,8 +1,16 @@
 use stylist::{style, yew::styled_component};
 use yew::prelude::*;
 
+#[derive(Properties, PartialEq)]
+pub struct Props{
+    pub children:String
+
+}
+
+
+
 #[styled_component]
-pub fn BBheading() -> Html{
+pub fn BBheading(props:&Props) -> Html{
 
     let center = style!{
         text-align: center;
@@ -11,7 +19,7 @@ pub fn BBheading() -> Html{
 
 
     html!{
-            <h1 class={center}> {"Todo Application"}</h1>
+            <h1 class={center}> {props.children.clone()} </h1>
 
     }
 }
