1063f7952b7d1aab0b2f8a970149932beb6d84f4
yrncollo <ngenocollins599@gmail.com>
2024-01-24 16:39:16 +0300

    implemented checkbox on todo list

--- src/components/molecules/tab.rs
+++ src/components/molecules/tab.rs
@@ -1,7 +1,7 @@
 use stylist::{style, yew::styled_component};
 use yew::{html, Html, Properties};
 
-use crate::components::atom::{listing::ListItems, heading_one::{BBheading, BBheaderLevel}};
+use crate::components::atom::{listing::ListItems, heading_one::{AAheading, AAheaderLevel}};
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -12,21 +12,25 @@ pub struct Props {
 pub fn tab(props:&Props) -> Html{
 
     let stylesheet = style!{
-        background-color: blue;
-        margin-right: 10px;
-        margin-left:10px;
-        margin-top:0px;
-        margin-bottom:10px;
-        border-radius:5px;
-        padding: 20px;
-        float: left;
-        width:260px;
+            background-color: blue;
+            margin-right: 10px;
+            margin-left:10px;
+            margin-top:0px;
+            margin-bottom:10px;
+            border-radius:5px;
+            padding: 20px;
+            float: left;
+            width:360px;
+            h2{
+                color: red;
+            }
+
     }.unwrap();
 
 
     html!{
         <div class={stylesheet}>
-            <BBheading level={BBheaderLevel::Two}> {props.children.clone()}</BBheading>
+            <AAheading level={AAheaderLevel::Two} center={true}> {props.children.clone()}</AAheading>
             <ListItems/>
         </div>
 
