90269fcae3439bdaaae7a2241c14e8a1fdd3e2e4
yrncollo <ngenocollins599@gmail.com>
2024-01-24 12:10:30 +0300

    created a function to list items

---
+++ src/components/atom/listing.rs
@@ -0,0 +1,50 @@
+use stylist::yew::styled_component;
+use yew::{function_component, html, Html, Properties};
+
+#[derive(Clone, PartialEq)]
+struct TodoItems{
+    id:usize,
+    title:String
+
+}
+
+#[derive(Properties, PartialEq)]
+struct ListProps{
+    todoitems:Vec<TodoItems>
+
+}
+
+#[styled_component]
+pub fn ListItems() -> Html{
+
+    let items = vec![
+        TodoItems{
+            id:1,
+            title:"Building todo application".to_string()
+        },
+
+        TodoItems{
+            id:2,
+            title:"How to cook".to_string()
+        },
+        TodoItems{
+            id:3,
+            title:"Reading rust".to_string()
+        }
+    ];
+
+
+    html!{
+        <Creatinglist todoitems={items}/>
+
+    }
+}
+
+#[function_component(Creatinglist)]
+fn creatinglist(ListProps { todoitems }: &ListProps) -> Html{
+
+    todoitems.iter().map(|list| html!{
+        <p key={list.id}> {format!("{}", list.title)}</p>
+    }).collect()
+    
+}
