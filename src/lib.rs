6fbfe17b0d1c3f57b46773b7bb18d0d806f56ef1
yrncollo <ngenocollins599@gmail.com>
2024-01-24 16:40:22 +0300

    frequent changes in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 use yew::prelude::*;
 mod components;
 
-use components::atom::heading_one::BBheading;
+use components::atom::heading_one::{AAheading, AAheaderLevel};
+use crate::components::molecules::tab::MMTab;
 
 #[function_component(App)]
 pub fn app() -> Html{
     
     html!{
-        <BBheading/>
+        <>
+            <AAheading center={true} level={AAheaderLevel::One}> {"Todo Application"}</AAheading>
+            <MMTab>{"Crafting/Ideas"}</MMTab>
+            <MMTab>{"Doing"}</MMTab>
+            <MMTab>{"Done"}</MMTab>
+        </>
+
 
     }
     
