7dc624912eee49027512554fa574292867eeb33e
yrncollo <ngenocollins599@gmail.com>
2024-01-30 15:42:41 +0300

    Starting again... Ready. Set. Go!!

--- src/components/atom/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod heading_one;
-pub mod buttons; 
-pub mod listing;
-pub mod checkbox;
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
 use yew::prelude::*;
-mod components;
 
-use components::atom::heading_one::{AAheading, AAheaderLevel};
-use crate::components::molecules::tab::MMTab;
 
 #[function_component(App)]
 pub fn app() -> Html{
     
     html!{
         <>
-            <AAheading center={true} level={AAheaderLevel::One}> {"Todo Application"}</AAheading>
-            <MMTab>{"Crafting/Ideas"}</MMTab>
-            <MMTab>{"Doing"}</MMTab>
-            <MMTab>{"Done"}</MMTab>
+            <p>{"Starting again"}</p>
         </>
 
 
