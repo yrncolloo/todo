4833e5735f2f415a6d4adfb710a667134ef0281e
yrncollo <ngenocollins599@gmail.com>
2024-02-01 16:14:07 +0300

    updated lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,26 @@
+use stylist::{style, yew::styled_component};
 use yew::prelude::*;
+use components::{sidebar::Sidebar, center_part::Centerpart};
 
+pub mod components;
 
-#[function_component(App)]
+#[styled_component(App)]
 pub fn app() -> Html{
     
+    let style = style!{
+        border:0.1px solid #F4F4F4;
+        background-color:#F4F4F4;
+        height:600px;
+        margin:20px;
+        border-radius:25px;
+    }.unwrap();
+
     html!{
-        <>
-            <p>{"Starting again"}</p>
-        </>
+        <div class={style}>
+            <Sidebar/>
+            <Centerpart/>
+
+        </div>
 
 
     }
