f3b983b328b96c833ee4e76c9aff4497c616606a
yrncollo <ngenocollins599@gmail.com>
2024-02-01 16:15:14 +0300

    completed sidebar

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -1,25 +1,42 @@
 use stylist::{style, yew::styled_component};
 use yew::{html, Html};
 use crate::components::ui::{title::Titles, title::TitleLevel};
-use crate::components::ui::search::Search;
+use crate::components::ui::{search::Search, tasks::Tasks, tags::Tags, button::But};
 
 #[styled_component(Sidebar)]
 pub fn sidebar() -> Html{
     let style = style!{
-        border:1px solid black;
         width:20%;
         height:86%;
         border-radius:25px;
-        margin:20px;
+        margin:2%;
         padding:20px;
         background-color:#EFECEC;
+        overflow:auto;
+        float:left;
+        //Well for some reason I added this and scrollbar disappeared wth
+        ::-webkit-scrollbar {
+              width: 10px;
+              height: 10px;
+        }
+
     }.unwrap();
+
    
+    let tasks:Vec<&str> = vec!["Upcoming", "Today", "Calendar", "Sticky Wall"];
+    let list:Vec<&str> = vec!["Personal", "Work", "List 1"];
     html!{
         <div class={style}>
             <Titles level={TitleLevel::Two}>{"Menu"}</Titles>
             <Search/>
-            <Titles level={TitleLevel::Five}>{"TASKS"}</Titles>
+            <Tasks title={"TASKS"}>{tasks}</Tasks>
+            <Tasks title={"LIST"}>{list}</Tasks>
+            <But>{"Add List"}</But>
+            <Tags/>
+            <br/>
+            <br/>
+            <But>{"Settings"}</But>
+            <But>{"Sign Out"}</But>
         </div>
 
     }
