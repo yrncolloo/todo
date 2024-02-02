16bd9bb27292ff66e3b9946768dab6d3e07c1e27
yrncollo <ngenocollins599@gmail.com>
2024-02-02 20:12:59 +0300

    fine tuned this shitty app

--- src/components/left_part.rs
+++ src/components/left_part.rs
@@ -1,13 +1,13 @@
 use stylist::{style, yew::styled_component};
 use yew::{html, Html};
 
+use crate::components::ui::{title::Titles, title::TitleLevel, tags::Tags};
 use crate::components::ui::description::Description;
 
 #[styled_component]
 pub fn Leftpart() -> Html{
     let style = style!{
 
-        border:0.1px solid black;
         float:left;
         height:86%;
         width:20%;
@@ -17,12 +17,19 @@ pub fn Leftpart() -> Html{
         padding:20px;
         border-radius:25px;
         background-color:#EFECEC;
+        .task{
+            border: 1px solid #7A7A7A;
+            padding: 10px;
+            border-radius: 10px;
+        }
     }.unwrap();
     html!{
 
         <div class={style}>
-            <h1>{"Tasks:"}</h1>
+            <Titles level={TitleLevel::Two}>{"Task:"}</Titles>
+            <p class={"task"}>{"Renew driver licence"}</p>
             <Description />
+            <Tags/>
         </div>
     }
     
--- src/components/ui/tags.rs
+++ src/components/ui/tags.rs
@@ -11,22 +11,29 @@ pub fn Tags() -> Html{
         }
         .tags{
             display:inline-flex;
+            flex-wrap:wrap;
         }
         width:auto;
     }.unwrap();
 
+    let tag:Vec<&str> = vec!["#Personal", "#Work", "#tag1", "#tag2", "#tag3", "#tag4"];
 
 
     html!{
         <div class={style}>
             <p class={"title"}>{"TAGS"}</p>
         <div class={"tags"}>
-            <But backgroundcolor={"red"}>{"#tag1"}</But>
-            <But>{"#tag1"}</But>
-            <But>{"#tag3"}</But>
+        {list_to_html(tag)}
         </div>
         </div>
 
     }
     
 }
+
+fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+    list.into_iter().map(|item | html!{
+        <But>{item}</But>
+    }).collect()
+}
+
