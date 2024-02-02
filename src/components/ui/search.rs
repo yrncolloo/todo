e534afa3c48eb1c92db3b27bfcedb9b49e78ac07
yrncollo <ngenocollins599@gmail.com>
2024-02-02 23:05:08 +0300

    implemented icons enum

--- src/components/ui/search.rs
+++ src/components/ui/search.rs
@@ -4,10 +4,21 @@ use yew::{html, Html, Properties};
 #[derive(Properties, PartialEq)]
 pub struct Props{
     pub placeholder:String,
+    pub icon:Icons
+}
+
+#[derive(PartialEq)]
+pub enum Icons {
+    Search,
+    Add
 }
 
 #[styled_component]
 pub fn Search(props:&Props) -> Html {
+    let icons = match props.icon {
+        Icons::Search => "fa fa-search".to_owned(),
+        Icons::Add => "fa fa-plus".to_owned()
+    };
     
     let style = style!{
 
@@ -18,13 +29,27 @@ pub fn Search(props:&Props) -> Html {
                     padding: 10px;
                     background-color: #EFECEC;
                     }
-                    background: url("/home/yrncollo/Pictures/wallpaper1.jpg");
+        .icon-input-container{
+            position:relative;
+            display:inline;
+            align-items:center;
+        }
+        .fa-search {
+                position: absolute;
+                top: 50%;
+                right: 10px; /* Adjust the position of the icon */
+                transform: translateY(-50%);
+                pointer-events: none; /* Prevent the icon from blocking clicks on the input */
+        }
     }.unwrap();
     
     html!{
 
         <div class={style}>
-            <input type="text" placeholder={props.placeholder.clone()}/>
+            <div class={"icon-input-container"}>
+                <input type="text" placeholder={props.placeholder.clone()}/>
+                <i class={icons}></i>
+            </div>
         </div>
     }
 }
