83423c5573e413863a1af84b138de318f647c032
yrncollo <ngenocollins599@gmail.com>
2024-02-02 16:10:53 +0300

    created center box to list items

---
+++ src/components/ui/center_box.rs
@@ -0,0 +1,34 @@
+use stylist::{style, yew::styled_component};
+use yew::{html, Html};
+
+#[styled_component]
+pub fn Box() -> Html{
+    let lists = vec!["Research content Idea", "Create database for guest authors", "Task 3"];
+    let style = style!{
+        padding: 10px;
+        font-size: 19px;
+        font-weight: medium;
+
+                .checkbox{
+                    margin-right: 10px;
+                }
+
+    }.unwrap();
+    html!{
+
+        <div class={style}>
+            {list_to_html(lists)}
+        </div>
+    }
+    
+}
+fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+    list.into_iter().map(|item | html!{
+        <>
+            <input type="checkbox" class={"checkbox"} name={"inp"}/>
+            <label for={"inp"}>{item}</label>
+            <hr/>
+        </>
+    }).collect()
+}
+
